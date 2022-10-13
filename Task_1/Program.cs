00cda36e8484ebf9b518fad1dc68cdf18ef87a3d
Vladislav <vasilevich.account@yandex.ru>
2022-10-14 00:00:00 +0300

    Выполнена задача 25

---
+++ Task_1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Программа с циклом, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.WriteLine("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+{
+  double Degree(int number1, int number2)
+  {
+    double result = 1; // Является ответом при записи чисел 0 и 0, так как блок кода "for" внутри "else" пропускается.
+    if (number2 < 0)
+    {
+      number2 = number2 * -1;
+      for (int counter = 1; counter <= number2; counter++)
+      {
+        result = result * number1;
+      }
+      result = 1 / result;
+    }
+    else
+    {
+      for (int counter = 1; counter <= number2; counter++)
+      {
+        result = result * number1;
+      };
+    }
+    Console.WriteLine($"Ответ: {result}");
+    return result;
+  }
+  Degree(a, b);
+}
