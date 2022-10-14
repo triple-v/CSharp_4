c8207a58ce4310578a48bb159a835a469dfe24f0
Vladislav <vasilevich.account@yandex.ru>
2022-10-14 18:06:24 +0300

    Выполнена задача 27

---
+++ Task_2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Программа, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите целое положительное число:");
+int num = Convert.ToInt32(Console.ReadLine());
+int SumNumbers(int number)
+{
+  int sum = 0;
+  if (number <= 0)
+  {
+    Console.WriteLine($"Введите число больше нуля");
+  }
+  else
+  {
+    while (number > 0)
+    {
+      sum += number % 10;
+      number = number / 10;
+    }
+    Console.WriteLine($"Сумма цифр в числе равна: {sum}");
+  }
+  return sum;
+}
+SumNumbers(num);
