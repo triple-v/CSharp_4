e7c7a82de58f9ca4299313a49e63d478d9c42799
Vladislav <vasilevich.account@yandex.ru>
2022-10-14 18:43:02 +0300

    Выполнена задача 25 и проведена работа над ошибками

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -4,28 +4,26 @@ Console.WriteLine("Введите первое число:");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число:");
 int b = Convert.ToInt32(Console.ReadLine());
+double Degree(int number1, int number2)
 {
-  double Degree(int number1, int number2)
+  double result = 1; // Является ответом при записи чисел 0 и 0, так как пропускается блок кода "for" внутри "else".
+  if (number2 < 0)
   {
-    double result = 1; // Является ответом при записи чисел 0 и 0, так как блок кода "for" внутри "else" пропускается.
-    if (number2 < 0)
+    number2 = number2 * -1;
+    for (int counter = 1; counter <= number2; counter++)
     {
-      number2 = number2 * -1;
-      for (int counter = 1; counter <= number2; counter++)
-      {
-        result = result * number1;
-      }
-      result = 1 / result;
+      result = result * number1;
     }
-    else
-    {
-      for (int counter = 1; counter <= number2; counter++)
-      {
-        result = result * number1;
-      };
-    }
-    Console.WriteLine($"Ответ: {result}");
-    return result;
+    result = 1 / result;
   }
-  Degree(a, b);
+  else
+  {
+    for (int counter = 1; counter <= number2; counter++)
+    {
+      result = result * number1;
+    };
+  }
+  Console.WriteLine($"Ответ: {result}");
+  return result;
 }
+Degree(a, b);
